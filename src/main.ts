b0c088fb134cb82a72e00291522ec5ac0f4af804
Jason Nordheim <jason.nordheim@gmail.com>
2023-10-23 10:21:08 -0600

    basic setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { Graph } from "./lib";
+import "./style.css";
 
+const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
+
+const graph = new Graph(canvas);
